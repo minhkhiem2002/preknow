22192dc30d36aea4a41a9aba8d5f63ea9bde55d9
minhkhiem2002 <leminhkhiem110@gmail.com>
2024-02-03 01:48:08 +0700

    Khiem new push

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Header from "../layouts/app.header"
+
+ 
+
+const Home = () => {
+    return ( 
+        <>
+        <Header/>
+        Home Page
+        </>
+    )
+}
+
+export default Home
